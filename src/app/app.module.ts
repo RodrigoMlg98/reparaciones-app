58a5b44f7e372abf31d9cb0f2d03d7ee04e852b8
RodrigoMlg98 <rexdante51@gmail.com>
2022-02-23 20:46:54 -0600

    crecion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponet } from './/header/header.componet';
+import { footerComponet } from './footer/footer.component';
+import '~ngx-owl-carousel-o/lib/styles/scss/owl.carousel';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponet,
+    footerComponet
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
