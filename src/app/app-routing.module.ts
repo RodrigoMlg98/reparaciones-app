d355bf92c8f4e241ff5a5ffab396f7e776954f8d
rauloco97 <rojo_pokemon_fuego@hotmail.com>
2022-03-07 23:20:43 -0600

    routing funcionando en servidros

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ServiciosComponentComponent } from './servicios-component/servicios-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ServicioComponentComponent } from './servicio-component/servicio-component.component';
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path:'home', component:HomeComponentComponent},
+  {path: 'servicios', component:ServiciosComponentComponent},
+  {path: 'servicio/:id', component:ServicioComponentComponent}
+
+];
+@NgModule({
+
+declarations: [
+    ServiciosComponentComponent,
+    HomeComponentComponent,
+    ServicioComponentComponent,
+                ],
+/*   imports: [BrowserModule, AppRoutingModule,RouterModule.forRoot(routes)], */
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { HeaderComponet } from './/header/header.componet';
 import { footerComponet } from './footer/footer.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { ServiciosComponentComponent } from './servicios-component/servicios-component.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ServicioComponentComponent } from './servicio-component/servicio-component.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
-const appRoutes:Routes=[
-  {path:'', component:HomeComponentComponent},
-  {path: 'servicios', component:ServiciosComponentComponent},
-{path: 'servicio/:id', component:ServicioComponentComponent}
-];
+
+
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent,
     HeaderComponet,
-    footerComponet,
-    ServiciosComponentComponent,
-    HomeComponentComponent,
-    ServicioComponentComponent,
-  
-  ],
-  imports: [
-    BrowserModule,
-    CarouselModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes)
-    
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    footerComponet,],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
