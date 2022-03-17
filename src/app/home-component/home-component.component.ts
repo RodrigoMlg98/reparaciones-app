5bf52e5a9405f1e77242b0c8e726f17b7c9f3892
RodrigoMlg98 <rexdante51@gmail.com>
2022-03-17 08:53:07 -0600

    animacion nosotros

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -18,24 +18,66 @@ export class HomeComponentComponent implements OnInit {
   animate_parrafoL = false;
   animate_parrafoR = false;
   fade = true;
+  fadepl = true;
+  fadepr = true;
+  /* NOSOTROS */
+  animate_nosotros = false;
+  animate_nosotropl = false;
+  animate_nosotropr =false;
+  faden = true;
+  fadenpl = true;
+  fadenpr = true;
+
+  
 
 
   @HostListener("document:scroll")
 /*   SCROLL DE INICIO */
 
   scrollfunction(){
-      if(document.body.scrollTop > 500|| document.documentElement.scrollTop > 500){
+      if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
           this.animate_title=true;
-/*           animate_parrafoL = true;
-          animate_parrafoR = true; */
+          this.animate_parrafoL = true;
+          this.animate_parrafoR = true;
           this.fade = false;
+          this.fadepl = false;
+          this.fadepr = false;
       }
+      
       else{
           this.animate_title=false; 
           this.fade = true;
+          this.fadepl = true;
+          this.fadepr = true;
       }
-  }
+    
+/*     MARCAS */
+if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+  this.animate_title=true;
+}
+else{
+  this.animate_title=false; 
+  this.fade = true;
+}
 
   /* SCROLL DE NOSOTROS */
-  
+
+    if(document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700){
+        this.animate_nosotros=true;
+        this.animate_nosotropl =true;
+        this.animate_nosotropr =true;
+        this.faden = false;
+        this.fadepl = false;
+        this.fadepr = false;
+    }
+    
+    else{
+        this.animate_nosotros=false; 
+        this.animate_nosotropl =false;
+        this.faden = true;
+        this.fadenpl = true;
+        this.fadenpr = true;
+    }
+  }
 }
+
