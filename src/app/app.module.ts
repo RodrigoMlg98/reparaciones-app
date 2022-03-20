eaec2c492f4f8533f287b56658f34469b331dcd4
rauloco97 <rojo_pokemon_fuego@hotmail.com>
2022-03-20 17:13:19 -0600

    edit formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -19,6 +20,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
   declarations: [AppComponent,
     HeaderComponet,
     footerComponet,
+    FormsModule
     
     
   ],
