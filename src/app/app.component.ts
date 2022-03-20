342976010bad5b1df9285d069e5aec1aa263dda5
rauloco97 <rojo_pokemon_fuego@hotmail.com>
2022-03-20 04:36:03 -0600

    rutas mejoradas, carrusel pogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
